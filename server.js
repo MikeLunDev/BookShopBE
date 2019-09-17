e2847344b523653b5b4d4b23521eb74ca8a9b1a2
Michele Lunati <lunatimichele@gmail.com>
2019-09-17 14:23:07 +0200

    end points for comment and books

---
+++ server.js
@@ -0,0 +1,30 @@
+const express = require("express");
+const bodyParser = require("body-parser");
+const bookRouter = require("./services/books");
+const commentRouter = require("./services/comments");
+const cors = require("cors");
+const listRoutes = require("express-list-endpoints");
+const server = express();
+server.set("port", process.env.PORT || 3450);
+
+server.use(bodyParser.json());
+/* 
+var whitelist = ["https://strivebooks.herokuapp.com", "http://localhost:3450/"];
+var corsOptions = {
+  origin: function(origin, callback) {
+    if (whitelist.indexOf(origin) !== -1) {
+      callback(null, true);
+    } else {
+      callback(new Error("Not allowed by CORS"));
+    }
+  }
+}; */
+
+server.use("/books", cors(), bookRouter);
+server.use("/books/comments", cors(), commentRouter);
+
+console.log(listRoutes(server));
+
+server.listen(server.get("port"), () => {
+  console.log("SERVER IS RUNNING ON " + server.get("port"));
+});
---
+++ services/comments.js
@@ -0,0 +1,47 @@
+const express = require("express");
+const fs = require("fs-extra");
+const shortid = require("shortid");
+
+const getComments = async () => {
+  var buffer = await fs.readFile("comments.json");
+  var items = buffer.toString();
+  return JSON.parse(items);
+};
+
+const saveComments = async books => {
+  await fs.writeFile("comments.json", books);
+};
+
+const router = express.Router();
+
+router.post("/:bookid", async (req, res) => {
+  var newComment = req.body;
+  newComment.Date = new Date();
+  newComment.BookID = req.params.id;
+  newComment._id = shortid.generate();
+  var listOfComments = await getComments();
+  listOfComments.push(newComment);
+  await saveComments(JSON.stringify(listOfComments));
+  res.send(newComment);
+});
+
+router.get("/:bookid", async (req, res) => {
+  var listOfComments = await getComments();
+  var filteredComments = listOfComments.filter(
+    comment => comment.BookID === req.params.id
+  );
+  if (filteredComments.length) {
+    res.send(filteredComments);
+  } else res.status(404).send(filteredComments);
+});
+
+router.delete("/:commentid", async (req, res) => {
+  var listOfComments = await getComments();
+  var filteredComments = listOfComments.filter(
+    comm => comm._id !== req.params.commentid
+  );
+  await saveComments(JSON.stringify(filteredComments));
+  res.send("deleted");
+});
+
+module.exports = router;
